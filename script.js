24e4576a5bb4419f9b670f194830b4fa09714a2f
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2026-07-05 01:59:33 +0500

    remove and add box logic successfully implemented

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let IncBtn = document.getElementById('inc')
 let DecBtn = document.getElementById('dec')
 let input = document.getElementById('inp')
+let rows = document.getElementsByClassName('row')
 let num = 3
 input.value = num
 let sound = new Audio("add.mp3")
@@ -20,11 +21,26 @@ function increase() {
     num = num + 1
     input.value = num
     check()
+    addBoxes()
     sound.play()
 }
 function decrease() {
     num = num - 1
     input.value = num
     check()
+    removeBoxes()
     sound.play()
+}
+function addBoxes(){
+    for(i=0;i<rows.length;i++){
+        box = document.createElement("div")
+        box.setAttribute("class",`b${num}`)
+        rows[i].appendChild(box)
+    }
+}
+function removeBoxes(){
+    for(i=0;i<rows.length;i++){
+        child=document.getElementsByClassName(`b${num+1}`)[0]
+        rows[i].removeChild(child)
+    }
 }
