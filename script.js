c4dc3bdc40e3c0e776eaad7b076f00c06662fd1f
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2026-07-09 11:10:14 +0500

    letters in respective boxes jugar

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ let IncBtn = document.getElementById('inc')
 let DecBtn = document.getElementById('dec')
 let input = document.getElementById('inp')
 let rows = document.getElementsByClassName('row')
+let letterBoxes = document.getElementsByClassName('letter')
 let num = 3
 let currKeyCount = 0
-let allKeyCount = num*6
+let allKeyCount = num * 6
 input.value = num
 let sound = new Audio("add.mp3")
 
-function check(){
-    if(num == 6){
-        IncBtn.setAttribute("disabled",true)
-    } else if(num ==3 ){
-        DecBtn.setAttribute("disabled",true)
-    } else{
+function check() {
+    if (num == 6) {
+        IncBtn.setAttribute("disabled", true)
+    } else if (num == 3) {
+        DecBtn.setAttribute("disabled", true)
+    } else {
         IncBtn.removeAttribute("disabled")
         DecBtn.removeAttribute("disabled")
     }
@@ -22,44 +23,46 @@ function check(){
 function increase() {
     num = num + 1
     input.value = num
-    allKeyCount = num*6
+    allKeyCount = num * 6
     check()
     addBoxes()
     sound.play()
+    console.log(letterBoxes);
 }
 function decrease() {
     num = num - 1
     input.value = num
-    allKeyCount = num*6
+    allKeyCount = num * 6
     check()
     removeBoxes()
     sound.play()
 }
-function addBoxes(){
-    for(i=0;i<rows.length;i++){
+function addBoxes() {
+    for (i = 0; i < rows.length; i++) {
         box = document.createElement("div")
-        box.setAttribute("class",`b${num}`)
+        box.setAttribute("class", `letter b${num}`)
         rows[i].appendChild(box)
     }
 }
-function removeBoxes(){
-    for(i=0;i<rows.length;i++){
-        child=document.getElementsByClassName(`b${num+1}`)[0]
+function removeBoxes() {
+    for (i = 0; i < rows.length; i++) {
+        child = document.getElementsByClassName(`b${num + 1}`)[0]
         rows[i].removeChild(child)
     }
 }
-window.addEventListener("keydown",((e)=>{
-    if(e.key == "Backspace"){
-        currKeyCount>0?currKeyCount=currKeyCount-1:currKeyCount=0
-        console.log(currKeyCount);
-    }else{
-        currKeyCount=currKeyCount+1
-        console.log(currKeyCount);
+window.addEventListener("keydown", ((e) => {
+    if (e.key == "Backspace") {
+        currKeyCount > 0 ? currKeyCount = currKeyCount - 1 : currKeyCount = 0
+    } else if (e.key == "a" || e.key == "b" || e.key == "c" || e.key == "d" || e.key == "e" || e.key == "f" || e.key == "g" || e.key == "h" || e.key == "i" || e.key == "j" || e.key == "k" || e.key == "l" || e.key == "m" || e.key == "n" || e.key == "o" || e.key == "p" || e.key == "q" || e.key == "r" || e.key == "s" || e.key == "t" || e.key == "u" || e.key == "v" || e.key == "w" || e.key == "x" || e.key == "y" || e.key == "z") {
+        if (currKeyCount < allKeyCount) {
+            currKeyCount = currKeyCount + 1
+            letterBoxes[currKeyCount - 1].innerHTML = e.key
+        }
     }
-    if(currKeyCount>0){
-        IncBtn.setAttribute("disabled",true)
-        DecBtn.setAttribute("disabled",true)
-    }else if(currKeyCount==0){
+    if (currKeyCount > 0) {
+        IncBtn.setAttribute("disabled", true)
+        DecBtn.setAttribute("disabled", true)
+    } else if (currKeyCount == 0) {
         IncBtn.removeAttribute("disabled")
         DecBtn.removeAttribute("disabled")
         check()
